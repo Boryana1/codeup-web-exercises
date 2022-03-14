a6654a29cc8051625f32930337cce2b1e7ba89bd
Boryana1 <boryana.dimova1@gmail.com>
2022-03-14 16:47:49 -0500

    exercises 14 Mar

---
+++ js/functions-lecture-notes.js
@@ -0,0 +1,26 @@
+//JS FUNCTIONS
+
+//SYNTAX
+
+//function --> keyword
+//name of the function
+// () --> potentially hold perimeters
+// (perimeter1, perimeter2) --> data required for the function to run
+//perimeters are passed in from the-outside-of-the-functions sources
+// {} --> define our function code block
+// {code goes here} ---> actual code to be run lives inside of the brackets
+
+// function printsMyName (){
+//     console.log('Boryana');
+// }
+// printsMyName();
+
+// TODO: write a function that prints out "firstNameAge"
+//  to the console replace firstName and age with your actual firstName and age
+
+function firstNameAge (){
+    var firstName = 'Boryana ';
+    var age = "29";
+    console.log(firstName + age)
+}
+firstNameAge();
