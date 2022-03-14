d942135fc8e2b74bc4227fcd14a988ae8286b89d
Boryana1 <boryana.dimova1@gmail.com>
2022-03-14 14:16:21 -0500

    js-with-html-preactice

---
+++ js/js-with-html-preactice.js
@@ -0,0 +1,6 @@
+var myName = 'Boryana';
+var myAge = '29';
+var isMonday = true;
+console.log(myName, myAge, isMonday);
+console.log (" I'm printing my name here: ", myName);
+console.log("my Name: " + myName + " my Age: " + myAge + " is it" + " monday? " + isMonday); //concatenation
