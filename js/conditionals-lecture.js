980e8ccbf36ea995730de2f9e99a4c1281ccfa2f
Boryana1 <boryana.dimova1@gmail.com>
2022-03-16 17:16:44 -0500

    update

---
+++ js/conditionals-lecture.js
@@ -0,0 +1,16 @@
+// TODO: write a conditional that prints "hey, she's my instructor" to the
+//  console if the name provided is "Laura", but if the name provided
+//  is "Kenneth", print out "oh, he's my instructor too!", if the name
+//  is neither Laura nor Kenneth, print out "um I don't know them"
+
+var name;
+if (name === "Laura") {
+    console.log("hey, she's my instructor");
+} else if (name === "Kenneth") {
+    console.log("oh, he's my instructor too!");
+} else {
+    console.log("um I don't know them");
+}
+console.log(name === "Kenneth");
+
+
