ee13e1c8e7a1c1d69c4588cdefd0fa66b2bc555b
Boryana1 <boryana.dimova1@gmail.com>
2022-03-22 11:31:47 -0500

    new js file

---
+++ js/while.js
@@ -0,0 +1,21 @@
+// 1.Create a file named while.js in the js directory.
+//
+// 2.Create a while loop that uses console.log() to create the output shown below.
+//
+//
+// 2
+// 4
+// 8
+// 16
+// 32
+// 64
+// 128
+// 256
+// 512
+// 1024
+// 2048
+// 4096
+// 8192
+// 16384
+// 32768
+// 65536
