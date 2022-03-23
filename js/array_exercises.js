5d35f38e1be0615c0df27f69cb13ebbf86e1ecd5
Boryana1 <boryana.dimova1@gmail.com>
2022-03-23 15:11:07 -0500

    during class exercise

---
+++ js/array_exercises.js
@@ -0,0 +1,5 @@
+var schoolGames = ["dodge-ball", "kickball", "hopscotch", "tag", "hide" +
+" and seek"];
+schoolGames.forEach(function (game){
+    console.log("My favorite game in school was: " + game);
+});
