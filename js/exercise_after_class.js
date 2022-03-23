157af0e7abfb8e785edc1281f1e988635a2dd6ce
Boryana1 <boryana.dimova1@gmail.com>
2022-03-23 10:45:38 -0500

    new FizzBuzz

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -19,3 +19,39 @@
     }
 }
 pickANumber();
+
+
+//TODO: Fizzbuzz Activity
+ // Print numbers 1-100
+ //if  # is divisible by 3 use Fizz
+ //if # is divisible by 5 use Buzz
+ // if # is divisible by 3 and 5 use FizzBuzz
+
+ function fizzBuzz() {
+     for (var i =1; i <= 100; i++) {
+         if (i % 3 === 0 && i % 5 === 0) {
+             console.log("FizzBuzz");
+         } else if (i % 3 === 0) {
+             console.log("Fuzz");
+         } else if (i % 5 === 0) {
+             console.log("Buzz");
+         } else {
+             console.log(i);
+         }
+     }
+
+ }
+ fizzBuzz();
+
+
+
+
+
+
+
+
+
+
+
+
+
