b2ede2d12893d4cf09b17e7de3b502071f4fc2de
Boryana1 <boryana.dimova1@gmail.com>
2022-03-25 21:48:29 -0500

    added a new variant for exercise 4

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,8 +5,8 @@
 // }
 // showMultiplicationTable(7);
 
-// for (var i = 1; i <= 10; i++) {
-//     var randomNumber = Math.floor(Math.random() * 200)+20;
+// for (var i = 1; i <= 100; i++) {
+//     var randomNumber = Math.floor(Math.random() * (200 - 20) + 20);
 //     if (randomNumber % 2 === 0) {
 //         console.log(randomNumber + " is even.");
 //     } else {
@@ -14,12 +14,32 @@
 //     }
 // }
 
+// EXERCISE 4:
+//
 // for(var i=1; i<=9; i++){
 //     var Num='';
 //     for(var j=0; j<i; j++){
 //         Num += i;
 //     }
 //     console.log(Num) }
+//
+
+// for (let i = 1; i <= 9; i++) {
+//     console.log(i)
+//     var num = '';
+//     num += i;
+//     for (let j = 1; j <= i ; j++) {
+//
+//     }
+// }
+
+for (let i = 1; i < 10; i++) {
+    var num = '';
+    for (let j = 1; j <= i; j++) {
+        num += i;
+    }
+    console.log(num);
+}
 
 // for (let i = 1; i < 10; i++) {
 //     let string = "";
