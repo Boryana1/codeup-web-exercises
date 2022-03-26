0e62a54c2ed4a27b51692e9789615e39df573c0d
Boryana1 <boryana.dimova1@gmail.com>
2022-03-25 21:47:47 -0500

    add a new variant for exercise 4

--- js/do_while.js
+++ js/do_while.js
@@ -2,7 +2,7 @@
 // An ice cream seller can't go home until she sells all of her cones.
 // First write enough code that generates a random number between 50
 // and 100 representing the amount of cones to sell before you start
-// your loop. Inside of the loop your code should generate another
+// your loop. Inside the loop your code should generate another
 // random number between 1 and 5, simulating the amount of cones
 // being bought by her clients. Use a do-while loop to log to the console
 // the amount of cones sold to each person. The below code shows how to
@@ -21,4 +21,18 @@
 
 
 //START:
-//
+var amtToSell = Math.floor(Math.random() * 50) + 50;
+
+do {
+    console.log("amtToSell BEFORE: ", amtToSell);
+
+
+    // This expression will generate a random number between 1 and 5
+    var amtBought = Math.floor(Math.random() * 5) + 1;
+
+    amtToSell -= amtBought;
+
+    console.log("amtToSell AFTER: ", amtToSell);
+    console.log("amtBought: ", amtBought);
+
+} while (amtToSell > amtBought)
