8ce2d0f6070b551e18fac3955de6536ae7b45d96
Boryana1 <boryana.dimova1@gmail.com>
2022-04-02 14:56:21 -0500

    new idea for exercise 9 from the second assessment

--- js/warm-up-exercises.js
+++ js/warm-up-exercises.js
@@ -18,3 +18,25 @@ var library = [
 for (let i = 0; i < library.length; i++){
     console.log(library[i].readingStatus)
 }
+
+//Problem 9: //FROM 2ND ASSESSMENT
+// Write a function named subtract that takes in two inputs. If both inputs provided are numeric or numeric strings, subtract will return the difference when the second input is subtracted from the first input. If one or both inputs is not numeric or numeric strings, subtract should return false.
+// subtract(10, 2)                     // 8
+// subtract(0, 0)                      // 0
+// subtract(-4, 1)                     // -5
+// subtract("10", 2)                   // 8
+// subtract(5, true)                   // false
+// subtract(true, false)               // false
+// subtract("Monday", "Tuesday")       // false
+// subtract()                          // false
+
+function isNumeric(x) {
+    return !isNaN(parseFloat(x));
+}
+function subtract(input1, input2) {
+    if (isNumeric(input1) && isNumeric(input2)) {
+        return input1 - input2;
+    } else {
+        return false;
+    }
+}
