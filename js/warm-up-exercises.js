50efe20cd24d1489a361e473a68294ff25a312d3
Boryana1 <boryana.dimova1@gmail.com>
2022-04-04 16:32:40 -0500

    new warm-up exercises about even numbers

--- js/warm-up-exercises.js
+++ js/warm-up-exercises.js
@@ -40,3 +40,17 @@ function subtract(input1, input2) {
         return false;
     }
 }
+
+//EXERCISE 4 APR 2022
+// Write a JS code to return an array of only the  Even numbers from the  given array
+// example input: [12,2,3,4,5,6,7,8,9]
+// expected output: [2,2,4,6,8]
+
+var numbers = [12,2,3,4,5,6,7,8,9];
+for (let i = 0; i < numbers.length ; i++) {
+    function if (i % 2 === 0) {
+    return i;
+    } else {
+        console.log(false);
+    }
+}
