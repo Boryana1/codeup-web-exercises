cca4a64152da48680cd5440341e0b853e62ccd0e
Boryana1 <boryana.dimova1@gmail.com>
2022-04-14 14:13:05 -0500

    js/warm-up-exercises.js

--- js/warm-up-exercises.js
+++ js/warm-up-exercises.js
@@ -269,3 +269,12 @@ for (let i = 1; i <= 50; i++) {
 // example input: [1, 2, 3], [3, 4, 5]
 // expected output:
 //     [ 1, 2, 3, 4, 5 ]
+
+function myArr(arr1, arr2) {
+    var result = arr1.concat(arr2);
+
+    result = [...new Set(result)];
+
+    return result.sort();
+}
+console.log(myArr([1,2,3],[3,4,5]));
