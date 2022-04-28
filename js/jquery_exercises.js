1e6636bfbaa7c7ae19eb8d1fb3f310b6d1a19f17
Boryana1 <boryana.dimova1@gmail.com>
2022-04-28 07:13:26 -0500

    done with jquery

---
+++ js/jquery_exercises.js
@@ -0,0 +1,32 @@
+// $(document).ready(function () {
+//     alert('The DOM has finished loading!');
+// });
+
+// ID SELECTORS
+
+// alert($('#header').html());
+// var menu = $('#menu-items').html();
+// console.log(menu);
+// alert(menu);
+
+// var pizza = $('#pizza').html();
+// alert(pizza);
+
+
+// CLASS SELECTORS
+
+// $('.codeup').css('border', '1px solid red');
+
+// ELEMENT SELECTORS
+//
+$('li').css('font-size', '20px');
+// $('h1, p, li').css('background-color', 'red');
+
+var h1Element = $("h1").html();
+var h1Element2 = $("#menu-items").html();
+alert(h1Element);
+alert(h1Element2);
+// Create a jQuery statement to alert the contents of your h1 element(s).
+//
+// MULTIPLE SELECTORS
+$('h1, p, li').css('background-color', 'red');
