ef353e44adafe615608067f7fd37e53401cdd49f
Boryana1 <boryana.dimova1@gmail.com>
2022-05-02 15:57:18 -0500

    js done

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,17 @@
+// $('document').ready(function() {
+//     $('#visibility-dd').click(function(event) {
+//         event.preventDefault();
+//         $('.invisible').toggleClass('.invisible');
+//     });
+// });
+"use strict"
+
 $('document').ready(function() {
-    $('#visibility-dd').click(function(event) {
+
+$('#visibility').click(function(event) {
         event.preventDefault();
-        $('.invisible').toggleClass('visibility');
+        $('.facts').toggleClass('invisible');
     });
 });
+
+console.log("This is js");
