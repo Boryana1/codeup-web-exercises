14fbc3fc14fb7bfc6e0fa9ecf1bc2aaf5aa78ba9
Boryana1 <boryana.dimova1@gmail.com>
2022-07-28 22:42:32 -0500

    Completed sorted arrays

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -61,6 +61,38 @@ pickANumber();
  }
 
  // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ var findMedianSortedArrays = function(nums1, nums2) {
+     if(nums1.length > nums2.length) {
+         return findMedianSortedArrays(nums2, nums1);
+     }
+
+     let m = nums1.length;
+     let n = nums2.length;
+     let starts = 0, ends = m;
+
+     while(starts <= ends) {
+         const partitionX = (starts + ends) >> 1;
+         const partitionY = ((m + n + 1) >> 1) - partitionX;
+
+         const maxLeftX = partitionX == 0 ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1]
+         const maxLeftY = partitionY == 0 ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1]
+
+         const minRightX = partitionX == nums1.length ? Number.POSITIVE_INFINITY : nums1[partitionX]
+         const minRightY = partitionY == nums2.length ? Number.POSITIVE_INFINITY : nums2[partitionY ]
+
+         if(maxLeftX <= minRightY && maxLeftY <= minRightX) {
+             const lowMax = Math.max(maxLeftX, maxLeftY);
+             if(( m + n ) % 2 == 1)
+                 return lowMax;
+             return (lowMax + Math.min(minRightX, minRightY)) / 2;
+         } else if(maxLeftX < minRightY) {
+             starts = partitionX + 1;
+         } else
+             ends = partitionX - 1;
+     }
+ };
+
+ // Problem Statement: Given a set of strings, find the longest common prefix.
 
 
 
