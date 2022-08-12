5c864346e088752ea12e46248b02c08a4f9f25bf
Boryana1 <boryana.dimova1@gmail.com>
2022-08-11 23:29:43 -0500

    Return sum of all digits exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -193,6 +193,14 @@ pickANumber();
  }
  printPattern(8);
 
-
+ function digitSum(num) {
+     var sum=0;
+     while(num!=0){
+         sum += num % 10;
+         num = Math.floor(num/10);
+     }
+     return sum;
+ }
+ console.log(digitSum(4367));
 
 
