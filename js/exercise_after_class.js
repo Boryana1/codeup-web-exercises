cd01ef8790040a59cb7d04b05a1c574993f996f7
Boryana1 <boryana.dimova1@gmail.com>
2022-08-11 23:28:39 -0500

    PrintPattern exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -182,6 +182,17 @@ pickANumber();
 
 
 
+ function printPattern(range) {
+     for(var i=1;i<=range;i++){
+         var str="";
+         for(var j=1;j<=i;j++){
+             str += j+" ";
+         }
+         console.log(str);
+     }
+ }
+ printPattern(8);
+
 
 
 
