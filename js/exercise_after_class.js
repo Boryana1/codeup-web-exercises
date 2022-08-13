404f5a59f133ed344d72074349582d5ab77995b9
Boryana1 <boryana.dimova1@gmail.com>
2022-08-12 22:14:27 -0500

    findProd exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -221,3 +221,16 @@ pickANumber();
  }
 
  console.log(zeroCount)
+
+
+ function findProd(arr1, arr2) {
+     var arrProd = arr1.length>arr2.length ? arr1 : arr2;
+     for(var i=0; i < Math.min(arr1.length, arr2.length); i++){
+         arrProd[i] = arr1[i] * arr2[i]; //Product of 2 array elements
+     }
+     return arrProd;
+ }
+ var arr1 = [3,45,23,78,34];
+ var arr2 = [4,2,34,4,12,1];
+
+ console.log(findProd(arr1,arr2));
