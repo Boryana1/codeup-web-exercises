6af030819f66f3b8883dea5576dbc3ce98a92723
Boryana1 <boryana.dimova1@gmail.com>
2022-08-12 22:13:45 -0500

    zeroCount exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -209,3 +209,15 @@ pickANumber();
      largest = arr[i]>largest ? arr[i]:largest; //Check largest number
  }
  console.log(largest)
+
+
+
+ var arr = [[0,1,1],[0,1,0],[1,0,0]];
+ var zeroCount = 0;
+ for(var i=0;i<arr.length;i++){
+     for(var j=0;j<arr[0].length;j++){
+         zeroCount = arr[i][j]==0 ? zeroCount+1 : zeroCount;
+     }
+ }
+
+ console.log(zeroCount)
