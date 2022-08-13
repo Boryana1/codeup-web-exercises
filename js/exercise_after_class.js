ced3a577ab60ff05bf78641291a689fb56fb95c5
Boryana1 <boryana.dimova1@gmail.com>
2022-08-12 22:09:19 -0500

    Largest array element exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -203,4 +203,9 @@ pickANumber();
  }
  console.log(digitSum(4367));
 
-
+ var arr = [2, 45, 3, 67, 34, 567, 34, 345, 123];
+ var largest = arr[0];
+ for(var i=0;i<arr.length;i++){
+     largest = arr[i]>largest ? arr[i]:largest; //Check largest number
+ }
+ console.log(largest)
