49eb1924175208f6bb163b7c935d8e32af7f3571
Boryana1 <boryana.dimova1@gmail.com>
2022-08-17 23:49:15 -0500

    isAnagram exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -244,3 +244,20 @@ pickANumber();
  var addSix = createBase(6);
  addSix(10);
  addSix(21);
+
+ var firstWord = "Mary";
+ var secondWord = "Army";
+
+ isAnagram(firstWord, secondWord); // true
+
+ function isAnagram(first, second) {
+     // For case insensitivity, change both words to lowercase.
+     var a = first.toLowerCase();
+     var b = second.toLowerCase();
+
+     // Sort the strings, and join the resulting array to a string. Compare the results
+     a = a.split("").sort().join("");
+     b = b.split("").sort().join("");
+
+     return a === b;
+ }
