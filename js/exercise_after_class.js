53f39458e50f490a1c8c960679e63196416504c5
Boryana1 <boryana.dimova1@gmail.com>
2022-08-24 23:17:13 -0500

    JavaScript Guide exercise.

--- js/exercise_after_class.js
+++ js/exercise_after_class.js
@@ -261,3 +261,15 @@ pickANumber();
 
      return a === b;
  }
+
+ var string = "Welcome to this Javascript Guide!";
+
+ // Output becomes !ediuG tpircsavaJ siht ot emocleW
+ var reverseEntireSentence = reverseBySeparator(string, "");
+
+ // Output becomes emocleW ot siht tpircsavaJ !ediuG
+ var reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
+
+ function reverseBySeparator(string, separator) {
+     return string.split(separator).reverse().join(separator);
+ }
